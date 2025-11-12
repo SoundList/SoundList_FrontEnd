68a6aed98f32246d2ea71be224c4bcdbd9c83f05
TomiNavasd <tomimanuelnavas@gmail.com>
2025-11-12 16:15:57 -0300

    Implementacion de rankings (semiterminada)

---
+++ Frontend/JS/Pages/rankings.js
@@ -0,0 +1,8 @@
+import { initializeRankingHandlers } from '../Handlers/rankingHandler.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Inicia toda la lógica de la página de rankings
+    initializeRankingHandlers();
+
+});
